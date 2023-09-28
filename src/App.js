42ba497395f567d66fc3ac96dfcd2c03c0dec4ed
Alex Sutherland <asutho@gmail.com>
2023-09-29 09:08:34 +1000

    Another update

--- src/App.js
+++ src/App.js
@@ -8,19 +8,7 @@ const FirebaseApp = initializeApp(firebaseConfig)
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
