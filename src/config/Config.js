15be5398edd02fae54bfcb1244d14a5fac25f099
Alex Sutherland <asutho@gmail.com>
2023-09-28 13:22:45 +1000

    Added Firebase

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmsWpmK4AVyBWsmDOmkTnEWffq35sTVOU",
+    authDomain: "movie-review-website-4fcd1.firebaseapp.com",
+    projectId: "movie-review-website-4fcd1",
+    storageBucket: "movie-review-website-4fcd1.appspot.com",
+    messagingSenderId: "428457600336",
+    appId: "1:428457600336:web:0dc42831fe6400b76e7855"
+  };
