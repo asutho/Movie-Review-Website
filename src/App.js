58045fc9b2c4a6a335c6391ce28abc9eebb22b0e
Alex Sutherland <asutho@gmail.com>
2023-09-29 09:31:21 +1000

    New button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { firebaseConfig } from './config/Config';
 import { initializeApp } from 'firebase/app';
 import './App.css';
+import { Test } from './components/Test';
 
 const FirebaseApp = initializeApp(firebaseConfig)
 
@@ -8,16 +9,7 @@ const FirebaseApp = initializeApp(firebaseConfig)
 function App() {
   return (
     <div className="App">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="btn btn-secondary">Secondary</button>
-      <button type="button" class="btn btn-success">Success</button>
-      <button type="button" class="btn btn-danger">Danger</button>
-      <button type="button" class="btn btn-warning">Warning</button>
-      <button type="button" class="btn btn-info">Info</button>
-      <button type="button" class="btn btn-light">Light</button>
-      <button type="button" class="btn btn-dark">Dark</button>
-
-      <button type="button" class="btn btn-link">Link</button>
+      <Test />
     </div>
   );
 }
---
+++ src/components/Test.js
@@ -0,0 +1,8 @@
+import Button from 'react-bootstrap/Button';
+
+export function Test () {
+
+    return (
+        <Button variant="Hello Button">Primary</Button>
+    )
+}
