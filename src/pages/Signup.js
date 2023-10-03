ef02d3c3771842d08c3822e60ac634374b82e56a
Alex Sutherland <asutho@gmail.com>
2023-10-04 10:48:16 +1100

    Added container

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,16 @@
+import Form from "react-bootstrap/Form"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+
 export function Signup ( props ) {
     return (
-        <div>
-            <h1>Sign up for an account</h1>
-        </div>
+        <Container fluid>
+            <Row>
+                <Col>One</Col>
+                <Col>Two</Col>
+            </Row>
+        </Container>
     )
 }
