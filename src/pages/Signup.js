1658fe95c1f6995707d65f79045f9c2911214bf2
Alex Sutherland <asutho@gmail.com>
2023-10-04 13:39:19 +1100

    Adding signup validation and context

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,9 +4,31 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from "react-bootstrap/Button"
 
+import { useState, useEffect } from "react"
 
 export function Signup ( props ) {
-    
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+    const [ validEmail, setValidEmail ] = useState(false)
+    const [ validPassword, setValidPassword] = useState(false)
+
+    useEffect( () => {
+        if( email.indexOf('@') > 0 ) {
+            setValidEmail(true)
+        }
+        else {
+            setValidEmail(false)
+        }
+    }, [email] )
+
+    useEffect( () => {
+        if( password.length > 7 ) {
+            setValidPassword(true)
+        }
+        else {
+            setValidPassword(false)
+        }
+    }, [password] )
     return (
         <Container fluid className="test">
             <Row>
@@ -18,14 +40,25 @@ export function Signup ( props ) {
                             <Form.Control
                             type="email"
                             placeholder="Enter email address"
-                            onChange={(val) => console.log(evt.target.value)}
+                            onChange={ (evt) => setEmail(evt.target.value) }
+                            value={email}
                             />
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter password" />
+                            <Form.Control
+                            type="password"
+                            placeholder="Enter password"
+                            onChange={ (evt) => setPassword(evt.target.value) }
+                            value={password}
+                            />
                         </Form.Group>
-                        <Button variants="primary" type="submit" className="my-2">
+                        <Button
+                            variants="primary" 
+                            type="submit"
+                            className="my-2"
+                            disabled = { ( validEmail && validPassword ) ? false: true }
+                        >
                             Submit
                         </Button>
                     </Form>
