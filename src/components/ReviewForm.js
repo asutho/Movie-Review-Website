03429076ad410286ccc33b2dabc544321b9cdf5a
Alex Sutherland <asutho@gmail.com>
2023-10-11 10:08:38 +1100

    Added star reviews

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,9 +2,16 @@ import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 
 export function ReviewForm(props) {
+    const SubmitHandler = ( event ) => {
+        event.preventDefault()
+        const data = new FormData(event.target)
+        const reviewTitle = data.get("title")
+        const reviewBody = data.get("body")
+        props.handler({title: reviewTitle, content: reviewBody})
+    }
     if( props.user) {
     return(
-        <Form>
+        <Form onSubmit={ SubmitHandler }>
         <h4>Add a Movie Rating!</h4>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Review Title</Form.Label>
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -11,13 +11,14 @@ import { FBStorageContext } from '../contexts/FBStorageContext';
 import { AuthContext } from '../contexts/AuthContext'
 import { FBAuthContext } from '../contexts/FBAuthContext';
 
-import { doc, getDoc, addDoc, collection } from 'firebase/firestore';
+import { doc, getDoc, addDoc, collection, getDocs } from 'firebase/firestore';
 import { ref, getDownloadURL } from 'firebase/storage';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export function Detail( props ) {
     const[ bookData, setBookData ] = useState()
     const[ auth, setAuth ] = useState()
+    const[ bookReviews, setBookReviews ] = useState([])
 
     let { bookId } = useParams()
 
@@ -36,11 +37,24 @@ export function Detail( props ) {
         }
     })
 
+    const getReviews = async () => {
+        const path = `Books/${bookId}/reviews`
+        const querySnapshot = await getDocs(collection(FBDb, path))
+        let reviews = []
+        querySnapshot.forEach( (item) => {
+            let review = item.data()
+            review.id = item.id
+            reviews.push(review)
+        })
+        setBookReviews(reviews)
+    }
+
     const bookRef = doc( FBDb, "Books", bookId)
     const getBook = async () => {
        let book = await getDoc( bookRef )
        if( book.exists() ) {
         setBookData( book.data() )
+        getReviews()
        }
        else {
         // no book exists with the ID
@@ -56,7 +70,7 @@ export function Detail( props ) {
     //function to handle review submission
     const ReviewHandler = async (reviewData) => {
     //create a document inside Firestore
-    const path = `books/${bookId}/reviews`
+    const path = `Books/${bookId}/reviews`
     const review = await addDoc(collection(FBDb, path), reviewData)
     }
 
@@ -88,8 +102,14 @@ export function Detail( props ) {
             </Row>
             <Row>
                 <Col>
-                <ReviewForm user={auth}/>
-                <h3>Write a Review!</h3>
+                <ReviewForm user={auth} handler={ReviewHandler}/>
+
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <ReviewForm user={auth} handler={ReviewHandler}/>
+
                 </Col>
             </Row>
         </Container>
